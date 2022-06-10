1cb86c5f977ddc331652e4a9bbc36615708bd9a3
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2022-06-10 17:29:12 +0300

    adsds

--- WPFDemoUI/ViewModels/ShellViewModel.cs
+++ WPFDemoUI/ViewModels/ShellViewModel.cs
@@ -1,34 +1,48 @@
-﻿using Caliburn.Micro;
-using DemoLibrary;
+﻿using DemoLibrary;
 using DemoLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Caliburn.Micro;
 using System.Threading.Tasks;
+
 namespace WPFDemoUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Screen
     {
         public BindableCollection<PersonModel> People { get; set; }
+
+
+
 
         public ShellViewModel()
         {
             DataAccess da = new DataAccess();
             People = new BindableCollection<PersonModel>(da.GetPeople());
         }
-
-
         private PersonModel _selectedPerson;
-
         public PersonModel SelectedPerson
         {
             get { return _selectedPerson; }
-            set {
-
+            set
+            {
+                _selectedPerson = value;
                 NotifyOfPropertyChange(() => SelectedPerson);
-                _selectedPerson = value; }
+
+            }
         }
+
+        private AddressModel _selectedAddress;
+
+        public AddressModel SelectedAddress
+        {
+            get { return _selectedAddress; }
+            set { _selectedAddress = value;
+                SelectedPerson.PrimaryAddress = value;
+                NotifyOfPropertyChange(() => SelectedPerson); }
+        }
+
 
     }
 }
