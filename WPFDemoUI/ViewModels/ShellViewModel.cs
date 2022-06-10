67347dff33a9b9f76d06d5a70becae92b35ecb0c
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2022-06-10 16:15:18 +0300

    Update ShellViewModel.cs

--- WPFDemoUI/ViewModels/ShellViewModel.cs
+++ WPFDemoUI/ViewModels/ShellViewModel.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace WPFDemoUI.ViewModels
 {
     public class ShellViewModel
@@ -18,5 +17,18 @@ namespace WPFDemoUI.ViewModels
             DataAccess da = new DataAccess();
             People = new BindableCollection<PersonModel>(da.GetPeople());
         }
+
+
+        private PersonModel _selectedPerson;
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set {
+
+                NotifyOfPropertyChange(() => SelectedPerson);
+                _selectedPerson = value; }
+        }
+
     }
 }
